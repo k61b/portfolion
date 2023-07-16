5e98a687f70f1658a33ed9b6dc585bc4f5e1c17d
Yusuf Akçekoce <yakcekoce@gmail.com>
2023-07-16 21:32:42 +0300

    feat: create sidebar

--- client/src/routes/dashboard.tsx
+++ client/src/routes/dashboard.tsx
@@ -1,38 +1,24 @@
-import { Button } from '@/components/ui/button'
 import { AuthContext } from '@/context/auth'
-import { fetcher } from '@/service'
 import { useContext } from 'react'
-import { Link, useNavigate } from 'react-router-dom'
 import { useTranslation } from 'react-i18next'
+import Sidebar from '@/components/dashboard/sidebar'
+import Table from '@/components/dashboard/table'
+import { Link } from 'react-router-dom'
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
-  const { setUser } = useContext(AuthContext)
-  const navigate = useNavigate()
   const { t } = useTranslation()
-
-  const handleLogout = () => {
-    fetcher('/api/v1/logout')
-      .then((data) => {
-        if (data.error) {
-          console.error(data.error)
-        } else {
-          setUser(null)
-          navigate('/')
-        }
-      })
-      .catch((err) => {
-        console.error(err.message)
-      })
-  }
 
   return (
     <div className="bg-slate-50">
       {user ? (
-        <div className="flex flex-col justify-center items-center h-screen">
-          <h1>{t('dashboard.title')}</h1>
-          <p>{t('dashboard.welcome', { name: user.username })}</p>
-          <Button onClick={handleLogout}>{t('dashboard.logout')}</Button>
+        <div className="grid md:grid-cols-[auto,1fr]">
+          <div className="mb-4 md:mb-0">
+            <Sidebar />
+          </div>
+          <div className="w-full ">
+            <Table />
+          </div>
         </div>
       ) : (
         <div className="flex flex-col justify-center items-center h-screen">
