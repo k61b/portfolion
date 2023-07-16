1f51813da677b2e87e658b5d8430d9997d6f8a75
Yusuf Akçekoce <94628640+yusufakcekoce@users.noreply.github.com>
2023-07-16 21:37:03 +0300

    feat: create sidebar (#47)

---
+++ client/src/components/dashboard/sidebar.tsx
@@ -0,0 +1,66 @@
+import { Avatar, AvatarImage } from '@radix-ui/react-avatar'
+import { Button } from '../ui/button'
+import { useContext } from 'react'
+import { AuthContext } from '@/context/auth'
+import { fetcher } from '@/service'
+import { useNavigate } from 'react-router-dom'
+import { useTranslation } from 'react-i18next'
+import { cn } from '@/lib/utils'
+
+export default function Sidebar() {
+  const { user } = useContext(AuthContext)
+  const { setUser } = useContext(AuthContext)
+  const { t } = useTranslation()
+  const navigate = useNavigate()
+
+  const handleLogout = () => {
+    fetcher('/api/v1/logout')
+      .then((data) => {
+        if (data.error) {
+          console.error(data.error)
+        } else {
+          setUser(null)
+          navigate('/')
+        }
+      })
+      .catch((err) => {
+        console.error(err.message)
+      })
+  }
+
+  return (
+    <div className="flex flex-row justify-around items-center md:items-stretch md:flex-col p-4 bg-slate-50 rounded shadow-xl h-max md:h-screen ">
+      <Avatar>
+        <AvatarImage
+          className="rounded-full w-36"
+          src="https://github.com/shadcn.png"
+        />
+      </Avatar>
+      <div className="mt-4 flex flex-col justify-center">
+        <h1 className="text-2xl font-semibold text-slate-900 mb-3">
+          {user?.username}
+        </h1>
+
+        <span className="text-slate-700 mb-1">
+          {t('dashboard.user.total', { value: user?.value })}
+        </span>
+
+        <span
+          className={cn('text-green-700', {
+            'text-red-700': user && user.profit_and_loss < 0,
+          })}
+        >
+          {t('dashboard.user.profit_and_loss', {
+            value: user?.profit_and_loss,
+          })}
+        </span>
+      </div>
+      <Button
+        onClick={handleLogout}
+        className="bg-slate-900 mt-auto w-max md:w-full"
+      >
+        {t('dashboard.logout')}
+      </Button>
+    </div>
+  )
+}
---
+++ client/src/components/dashboard/table.tsx
@@ -0,0 +1,9 @@
+export default function Table() {
+  return (
+    
+      <div className="p-4 bg-slate-900 rounded shadow-xl">
+        <p className="text-white">right</p>
+      </div>
+   
+  )
+}
