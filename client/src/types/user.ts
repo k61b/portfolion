48802d6692754bda7528f842cd37d9c5a006cd37
Kayra Berk Tuncer <kayrabtuncer@gmail.com>
2023-07-23 18:56:51 +0300

    refactor: do not return bookmarks in users (#63)

--- client/src/types/user.ts
+++ client/src/types/user.ts
@@ -1,13 +1,6 @@
 export interface IUser {
   avatar: string
   username: string
-  bookmarks: Array<Bookmark>
   value: number
   profit_and_loss: number
-}
-
-interface Bookmark {
-  symbol: string
-  added_price: number
-  pices: number
 }
